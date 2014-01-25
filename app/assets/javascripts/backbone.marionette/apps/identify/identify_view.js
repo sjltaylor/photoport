a52db8b50143d0b5a83edbfbdbaf132c5f4ef64c
Sam Taylor <sjltaylor@gmail.com>
2014-01-25 23:13:53 +0000

    review the styling and language of identity view

--- app/assets/javascripts/backbone.marionette/apps/identify/identify_view.js
+++ app/assets/javascripts/backbone.marionette/apps/identify/identify_view.js
@@ -7,8 +7,14 @@ PhotoportCMS.module('Identify', function (Identify, PhotoportCMS, Backbone, Mari
     events: {
       'click .js-cancel': 'onCancelSave'
     },
+    ui: {
+      emailField: 'form input[name="email"]'
+    },
     onCancelSave: function () {
       this.trigger('cancel-save');
+    },
+    onShow: function () {
+      this.ui.emailField.focus();
     }
   });
 });
